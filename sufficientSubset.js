e41835f0419faead4ce45eb269db23bc15bf53ba
Sharath vasa <sharathvasa@gmail.com>
2021-04-30 08:31:36 -0400

    Feat: Added solution to the problem sufficientSubset

---
+++ sufficientSubset.js
@@ -0,0 +1,59 @@
+/*
+Given the root of a binary tree, consider all root to leaf paths: paths from the root to any leaf.  (A leaf is a node with no children.)
+
+A node is insufficient if every such root to leaf path intersecting this node has sum strictly less than limit.
+
+Delete all insufficient nodes simultaneously, and return the root of the resulting binary tree.
+
+ 
+
+Example 1:
+
+
+Input: root = [1,2,3,4,-99,-99,7,8,9,-99,-99,12,13,-99,14], limit = 1
+
+Output: [1,2,3,4,null,null,7,8,9,null,14]
+Example 2:
+
+
+Input: root = [5,4,8,11,null,17,4,7,1,null,null,5,3], limit = 22
+
+Output: [5,4,8,11,null,17,4,7,null,null,null,5]
+ 
+
+Example 3:
+
+
+Input: root = [1,2,-3,-5,null,4,null], limit = -1
+
+Output: [1,null,-3,4]
+ 
+
+Note:
+
+The given tree will have between 1 and 5000 nodes.
+-10^5 <= node.val <= 10^5
+-10^9 <= limit <= 10^9
+*/
+var sufficientSubset = function(root, limit) {
+    let dummyNode = new TreeNode(0);
+    dummyNode.right = root;
+    deleteTree(dummyNode,0,limit);
+    return dummyNode.right;
+};
+
+const deleteTree = (node, currentSum, target) => {
+    if(!node) return true;
+    if(!node.left && !node.right) {
+        return  node.val + currentSum < target;
+    }
+    let left = deleteTree(node.left, currentSum + node.val, target);
+    let right = deleteTree(node.right, currentSum + node.val, target);
+    if(left) {
+        node.left = null;
+    }
+    if(right) {
+        node.right = null;
+    }
+    return left && right;
+}
